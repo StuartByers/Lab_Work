76ae46416c83896e8fb3b9e20a6273642a2640de
StuartByers <stuart.byers@outlook.com>
2023-03-15 16:37:40 +0000

    finsihed extensions

---
+++ order_shop/models/order.py
@@ -0,0 +1,10 @@
+class Order():
+
+    def __init__(self, customer_name,model,size,quantity,order_date, delivery_date):
+        self.customer_name = customer_name
+        self.model = model
+        self.size = size
+        self.quantity = quantity
+        self.order_date = order_date
+        self.delivery_date = delivery_date
+        
---
+++ order_shop/models/order_list.py
@@ -0,0 +1,9 @@
+from models.order import *
+
+order1 = Order("Toby", "SB Blazer", 9.5, 1, "18/4/97", "22/4/97")
+order2 = Order("Stuart", "Jordan 4", 9, 1, "12/10/81", "29/10/81")
+
+# extension
+order3 = Order("Keith", "Nike Vomero", 10, 1, "15/3/23", "21/3/23")
+
+order_list = [order1, order2, order3]
